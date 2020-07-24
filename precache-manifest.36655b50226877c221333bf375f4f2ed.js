5a881aa595d9557370d56811581131f1a1973065
JandersonSR <jandersonrodriguesir@gmail.com>
2020-07-23 21:18:11 -0300

    Updates

---
+++ precache-manifest.36655b50226877c221333bf375f4f2ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d8fbec0843d146a99864f91a5f8e46",
+    "url": "/JandersonSR/robo/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/JandersonSR/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e54b38c92e5858c7295b",
+    "url": "/JandersonSR/robo/static/css/main.9b93e6ec.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/JandersonSR/robo/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JandersonSR/robo/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54b38c92e5858c7295b",
+    "url": "/JandersonSR/robo/static/js/main.a2f4d9f8.chunk.js"
+  },
+  {
+    "revision": "5959025701d0f2077986",
+    "url": "/JandersonSR/robo/static/js/runtime-main.77367572.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/JandersonSR/robo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
